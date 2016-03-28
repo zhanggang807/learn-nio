b900ffd02153ba93f6bcbb4c970697ebc9880be2
zhanggang13 <zhanggang3@jd.com>
2016-03-28 18:20:32 +0800

    datagrame channel time client

--- src/main/java/org/dean/learn/nio/channel/TimeClient.java
+++ src/main/java/org/dean/learn/nio/channel/TimeClient.java
@@ -2,7 +2,11 @@ package org.dean.learn.nio.channel;
 
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 import java.nio.channels.DatagramChannel;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -33,7 +37,7 @@ public class TimeClient {
     private static final long DIFF_1900 = 2208988800L;
 
     protected int port = DEFAULT_TIME_PORT;
-    protected List remoteHosts;
+    protected List<InetSocketAddress> remoteHosts;
     protected DatagramChannel channel;
 
     public TimeClient(String[] args) throws Exception {
@@ -51,13 +55,98 @@ public class TimeClient {
     }
 
     // Send time requests to all the supplied hosts
-    protected void (){
-
+    protected void sendRequests() throws Exception{
+        ByteBuffer buffer = ByteBuffer.allocate(1);
+        Iterator it = remoteHosts.iterator();
+        while (it.hasNext()){
+            InetSocketAddress sa = (InetSocketAddress)it.next();
+            System.out.println("Requesting time from " + sa.getHostName() + ":" + sa.getPort());
+            // Make it empty (see RFC868)
+            buffer.clear().flip();
+            // Fire and forget
+            channel.send(buffer, sa);
+        }
     }
 
+    // Receive any replies that arrive
+    public void getReplies() throws Exception {
+        // Allocate a buffer to hold a long value
+        ByteBuffer longBuffer = ByteBuffer.allocate(8);
+        // Assure big-endian (network) byte order
+        longBuffer.order(ByteOrder.BIG_ENDIAN);
+        // Zero the whole buffer to be sure
+        longBuffer.putLong(0, 0);
+        // Position to first byte of the low-order 32bits
+        longBuffer.position(4);
+        // Slice the buffer; gives view of the low-order 32bits
+        ByteBuffer buffer = longBuffer.slice();
+        int expect = remoteHosts.size();
+        int replies = 0;
+        System.out.println("");
+        System.out.println("Waiting for replies...");
+        while (true){
+            InetSocketAddress sa;
+            sa = receivePacket(channel, buffer);
+            buffer.flip();
+            replies ++;
+            printTime(longBuffer.getLong(0), sa);
+            if (replies == expect) {
+                System.out.println("All packets answered");
+                break;
+            }
+            // Some replies haven't shown up yet
+            System.out.println("Received " + replies + " of " + expect + " replies");
+        }
+    }
 
+    // Print info about a received time reply
+    protected void printTime(long remote1900, InetSocketAddress sa){
+        // local time as seconds since Jan 1, 1970
+        long local = System.currentTimeMillis() / 1000;
+        // remote time as seconds since Jan 1, 1970
+        long remote = remote1900 - DIFF_1900;
+        Date remoteDate = new Date(remote * 1000);
+        Date localDate = new Date(local * 1000);
+        long skew = remote - local;
+        System.out.println("Reply from " + sa.getHostName() + ":" + sa.getPort());
+        System.out.println(" there: " + remoteDate);
+        System.out.println(" here: " + localDate);
+        System.out.println("skew: ");
+        if (skew == 0) {
+            System.out.println("none");
+        } else if (skew > 0){
+            System.out.println(skew + " seconds ahead");
+        } else {
+            System.out.println((-skew) + " seconds behind");
+        }
+    }
 
-    public static void main(String[] args) {
+    protected void parseArgs(String[] args){
+        remoteHosts = new LinkedList<InetSocketAddress>();
+        for (int i = 0; i < args.length; i++) {
+            String arg = args[i];
+            // Send client requests to the given port
+            if (arg.equals("-p")) {
+                i++;
+                this.port = Integer.parseInt(args[i]);
+                continue;
+            }
+            // Create an address object for the hostname
+            InetSocketAddress sa = new InetSocketAddress(arg, port);
+            // Validate that it has an address
+            if (sa.getAddress() == null) {
+                System.out.println("Cannot resolve address: " + arg);
+                continue;
+            }
+            remoteHosts.add(sa);
+        }
+    }
+
+    // -------------------------------------------------------------------------------------
+    public static void main(String[] args) throws Exception {
+        TimeClient client = new TimeClient(args);
+        client.sendRequests();
+        client.getReplies();
     }
 
 }
